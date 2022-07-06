b97f59ced3e50f70cbe4d8611ea683fd7956daaa
Benjamin <benjamin@sevengames.xyz>
2022-07-06 16:32:27 +0100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TicTacToe {
+    class Program {
+
+        static string board = "╔═══╦═══╦═══╗\n║   ║   ║   ║\n╠═══╬═══╬═══╣\n║   ║   ║   ║\n╠═══╬═══╬═══╣\n║   ║   ║   ║\n╚═══╩═══╩═══╝\n";
+
+        static void Main(string[] args) {
+            Console.WriteLine(board);
+        }
+    }
+}
